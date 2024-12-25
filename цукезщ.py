a4f3c4b72231953e57a53baab9efb073096b7971
Egorius0202 <egorkononyhin37@gmail.com>
2024-12-25 11:08:22 +0700

    Update цукезщ.py

--- цукезщ.py
+++ цукезщ.py
@@ -1,8 +1,9 @@
 Кононыхин Егор мф-71
 import pandas as pd
-
 df_excel = pd.read_excel('data.xlsx')
-
-print(df_excel.info())
-
-print(df_excel.[["column1","column2"]])
+print(df_excel.head())
+print(df_excel.sample(5))
+print(df_excel.tail(5))
+num_1=int(input("напиши номер строки"))
+df_excel = pd.read_excel('data.xlsx',index_col=num_1)
+print(df_excel)
