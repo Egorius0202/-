3a52d257a032b521f3d378469adf1fb44579c6b8
Egorius0202 <egorkononyhin37@gmail.com>
2025-02-05 11:38:38 +0700

    Update цукезщ.py

--- цукезщ.py
+++ цукезщ.py
@@ -14,3 +14,19 @@ num_2=int(input("напиши номер столбца"))
 df= pd.read_excel('оценка1.xlsx')
 print(df)
 print(df.iloc[num_1, num_2])
+05.02.2025
+ import math
+import pandas as pd
+num_1=int(input("введите 1 если вывести весь дата сэт,введите 2 вывести конкретную строчку, введите 3 вывести столбец, введите 4 вывести ячейку"))
+num_2=1
+num_3=2
+num_4=3
+num_5=4
+if num_1  == num_2 :
+ df_excel = pd.read_excel('Оценки (1).xlsx')
+print(df_excel.head())
+if num_1 == num_5 :
+ num_6=int(input("напиши номер строки"))
+num_7=int(input("напиши номер столбца"))
+df= pd.read_excel('оценка (1).xlsx')
+print(df.iloc[num_6, num_7])
