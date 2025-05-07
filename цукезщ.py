0a3818ffacf7c58779a59cb1186c83d94eb7d503
Egorius0202 <egorkononyhin37@gmail.com>
2025-05-07 11:23:11 +0700

    Update цукезщ.py

--- цукезщ.py
+++ цукезщ.py
@@ -1,32 +1,49 @@
 Кононыхин Егор мф-71
+#задача 3
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.colors import LightSource
+
+# Создаем координаты X, Y
+X = np.linspace(-5, 5, 100)
+Y = np.linspace(-5, 5, 100)
+X_grid, Y_grid = np.meshgrid(X, Y)
+
+# Функция рельефа (горы и холмы)
+Z_grid = np.sin(np.sqrt(X_grid**2 + Y_grid**2)) * 2 + np.cos(Y_grid) * 1.5
+
+# Добавляем источник света
+ls = LightSource(azdeg=315, altdeg=45)
+
+# Используем plt.cm.terrain вместо 'terrain'
+shaded_Z = ls.shade(Z_grid, cmap=plt.cm.terrain, vert_exag=0.2, blend_mode='soft')
+
+# Создаем 3D-график
+fig = plt.figure(figsize=(8, 6))
+ax = fig.add_subplot(111, projection='3d')
+
+# Отображаем рельеф с тенью
+ax.plot_surface(X_grid, Y_grid, Z_grid, facecolors=shaded_Z, rstride=1, cstride=1)
+
+# Настройки осей
+ax.set_xlabel("X координата")
+ax.set_ylabel("Y координата")
+ax.set_zlabel("Высота (Z)")
+
+plt.show()
+
+#Задача 2
 import pandas as pd
-df_excel = pd.read_excel('data.xlsx')
-print(df_excel.head())
-print(df_excel.sample(5))
-print(df_excel.tail(5))
-num_1=int(input("напиши номер строки"))
-df_excel = pd.read_excel('data.xlsx',index_col=num_1)
-print(df_excel)
-22.01.2025
+import matplotlib.pyplot as plt
+table = pd.read_excel('data.xlsx')
+x = table.values[:, 0]
+y = table.values[:, 1]
+plt.figure(figsize=(15, 7))
+plt.plot(x, y)
+plt.show()
+#задача 1
+
 import pandas as pd
-num_1=int(input("напиши номер строки"))
-num_2=int(input("напиши номер столбца"))
-df= pd.read_excel('оценка1.xlsx')
-print(df)
-print(df.iloc[num_1, num_2])
-05.02.2025
- import math
-import pandas as pd
-num_1=int(input("введите 1 если вывести весь дата сэт,введите 2 вывести конкретную строчку, введите 3 вывести столбец, введите 4 вывести ячейку"))
-num_2=1
-num_3=2
-num_4=3
-num_5=4
-if num_1  == num_2 :
- df_excel = pd.read_excel('Оценки (1).xlsx')
-print(df_excel.head())
-if num_1 == num_5 :
- num_6=int(input("напиши номер строки"))
-num_7=int(input("напиши номер столбца"))
-df= pd.read_excel('оценка (1).xlsx')
-print(df.iloc[num_6, num_7])
+num1=int(input("напиши номер строки"))
+print(df.iloc[num1])
